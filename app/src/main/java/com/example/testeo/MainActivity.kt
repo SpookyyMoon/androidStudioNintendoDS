df49424733dbdd5e161a0e8c0cb2336a4139bac1
SpookyyMoon <spookyluna666@gmail.com>
2025-10-01 19:02:48 +0200

    Inicio video al presionar el botón, constraits

--- app/src/main/java/com/example/testeo/MainActivity.kt
+++ app/src/main/java/com/example/testeo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.testeo
 
-import android.media.session.MediaController
+import android.widget.MediaController
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,7 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val videoView: VideoView = findViewById(R.id.videoSuperior)
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.inicio);
+
+        val botonStart = findViewById<Button>(R.id.boton_start)
+        botonStart.setOnClickListener {
+            val videoView: VideoView = findViewById(R.id.videoSuperior)
+            val packageName = "android.resource://${packageName}/${R.raw.inicio}"
+            val uri = Uri.parse(packageName)
+            videoView.setVideoURI(uri)
+
+            val mediaController = MediaController(this@MainActivity)
+            mediaController.setAnchorView(videoView)
+            videoView.setMediaController(mediaController)
+
+            videoView.start()
+        }
     }
 }
+
